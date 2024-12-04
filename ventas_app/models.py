312fbefe6ed10533b4919889206ac59474408ebe
sergio-altamirano <sergiodamianaltamirano@gmail.com>
2024-12-04 08:18:27 -0600

    Add files via upload

---
+++ ventas_app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Ventas(models.Model):
+    id_venta=models.SmallIntegerField(primary_key=True)
+    id_trabajador=models.SmallIntegerField()
+    id_producto=models.SmallIntegerField()
+    id_cliente=models.SmallIntegerField()
+    fecha_venta=models.DateField()
+    id_sucursal=models.SmallIntegerField()
+    cantidad=models.SmallIntegerField()
+    pago=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.pago
